290e2a90b5b53335ff58099161a824e79ec6612e
ivan montañes <ivantuckp12345@gmail.com>
2024-04-23 00:11:10 -0300

    punto 3 terminado

--- tp3_3.c
+++ tp3_3.c
@@ -2,29 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+char **crearArreglo(int cantidad);
+void mostrarNombres(char **nombres,int cantidad);
+void liberarMemoria(char **nombres,int cantidad);
+
 int main()
 {
+    char **nombres;
     int cantidadNombres;
     printf("ingrese la cantidad de nombres que desea cargar:");
     fflush(stdin);
-    scanf("%d", &cantidadNombres);
-    char **nombres = (char **)malloc(cantidadNombres * sizeof(char *));
+    scanf("%d",&cantidadNombres);
+    nombres = crearArreglo(cantidadNombres);
+    mostrarNombres(nombres,cantidadNombres);
+    liberarMemoria(nombres,cantidadNombres);
+    return 0;
+}
+
+char **crearArreglo(int cantidad)
+{
+    char **nombres = (char**)malloc(cantidad*sizeof(char*));
     char buff[50];
-    for(int i = 0; i < cantidadNombres; i++){
+    for(int i = 0; i < cantidad; i++){
         printf("ingrese un nombre:");
         fflush(stdin);
         scanf("%s",buff);
         nombres[i] = (char *)malloc(strlen(buff) * sizeof(char) + 1);
         strcpy(nombres[i],buff);
     }
-    for(int i = 0; i < cantidadNombres; i++){
+    return nombres;
+}
+
+void mostrarNombres(char **nombres,int cantidad)
+{
+    for(int i = 0; i < cantidad; i++){
         printf("nombre: ");
         puts(nombres[i]);
     }
-    
-    for(int i = 0; i < cantidadNombres; i++){
+}
+
+void liberarMemoria(char **nombres,int cantidad)
+{
+    for(int i = 0; i < cantidad; i++){
         free(nombres[i]);
     }
     free(nombres);
-    return 0;
 }
