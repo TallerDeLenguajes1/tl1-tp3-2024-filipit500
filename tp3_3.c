88b5e2e48597288dc027b9320ae8cb4b2fc0863d
ivan montañes <ivantuckp12345@gmail.com>
2024-04-22 23:06:42 -0300

    punto 5

--- tp3_3.c
+++ tp3_3.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define PALABRAS 5
 
 int main()
 {
-    char **nombres = (char **)malloc(PALABRAS * sizeof(char *));
+    int cantidadNombres;
+    printf("ingrese la cantidad de nombres que desea cargar:");
+    fflush(stdin);
+    scanf("%d", &cantidadNombres);
+    char **nombres = (char **)malloc(cantidadNombres * sizeof(char *));
     char buff[50];
-    for(int i = 0; i < PALABRAS; i++){
+    for(int i = 0; i < cantidadNombres; i++){
         printf("ingrese un nombre:");
         fflush(stdin);
         scanf("%s",buff);
         nombres[i] = (char *)malloc(strlen(buff) * sizeof(char) + 1);
         strcpy(nombres[i],buff);
     }
-    for(int i = 0; i < PALABRAS; i++){
+    for(int i = 0; i < cantidadNombres; i++){
         printf("nombre: ");
         puts(nombres[i]);
     }
     
-    for(int i = 0; i < PALABRAS; i++){
+    for(int i = 0; i < cantidadNombres; i++){
         free(nombres[i]);
     }
     free(nombres);
     return 0;
-    
 }
